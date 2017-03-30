2546a343399df8d64e002650e22fa408a4d6fb51
bhoot180 <bhoot1806@gmail.com>
2017-03-30 18:05:35 -0400

    Add files via upload

---
+++ Homework/mixingDataTypes/MixingDataTypes.java
@@ -0,0 +1,30 @@
+// Brandon Hoots
+// This code is an example of mixing data types in 
+// a mathematical formula 
+
+
+package mixingdatatypes;
+
+public class MixingDataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Declaring the variables
+		int a = 5;
+		float b = (float) 10.2;
+		
+		
+		//Computation
+		float c = a * b; 
+		
+		
+		//Printing the results
+		System.out.println("The answer is:");
+		System.out.println(c);
+
+	
+	
+	}
+
+}

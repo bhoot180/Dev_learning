cd2c50fcee8f50e9fea2be6288fe6a563881ad43
bhoot180 <bhoot1806@gmail.com>
2017-03-30 17:58:44 -0400

    Add files via upload

---
+++ udacity/day/Day.java
@@ -0,0 +1,20 @@
+package day;
+
+public class Day {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String month = "Nov";
+		int day = 28;
+		int year = 1964;
+		int flightTime = 228;
+		
+		String launchDate = month + ' ' + day + ' ' + year;
+		String arivalDate = launchDate + flightTime;
+
+		
+		System.out.println(date);
+	}
+
+}

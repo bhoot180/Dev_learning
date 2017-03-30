37b72b829f1226aed0b3d0d35a756f1d47efbdd0
bhoot180 <bhoot1806@gmail.com>
2017-03-30 18:04:45 -0400

    Add files via upload

---
+++ Homework/dataType/DataTypes.java
@@ -0,0 +1,23 @@
+//Brandon Hoots
+//Code prints and integer and float value below a string
+
+
+package datatypes;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 5;
+		float b = (float) 10.2;
+		
+		
+		System.out.println("Integer and float value:");
+		System.out.println(a);
+		System.out.println(b);
+	
+	
+	}
+
+}

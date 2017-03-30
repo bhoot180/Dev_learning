ebb4fc5bf5be930fd4b948e4960463c9f0cffbb9
bhoot180 <bhoot1806@gmail.com>
2017-03-30 18:12:52 -0400

    Add files via upload

---
+++ useCases/dataType/DataType.java
@@ -0,0 +1,36 @@
+package dataType;
+
+public class DataType {
+
+
+	  boolean a = true;
+	  byte b = 1;
+	  char c = 'C';
+	  double d = 12.94;
+	  float f = 10.3f;
+	  int i = 16;
+	  long l = 67;
+	  short s = 4;
+	  String string = "hello";
+	 
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		DataType dt = new DataType();
+		
+		
+		System.out.println("Bool :" + dt.a);
+		System.out.println("Byte :" + dt.b);
+		System.out.println("Character:" + dt.c);
+		System.out.println("Double :" + dt.d);
+		System.out.println("Float :" + dt.f);
+		System.out.println("Integer :" + dt.i);
+		System.out.println("Long :" + dt.l);
+		System.out.println("Short :" + dt.s);
+		System.out.println("String :" + dt.string);
+			  }	
+	}

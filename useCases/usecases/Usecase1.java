7325bdd74aed2e5c91e8cfe3105f252ebe2d46ea
bhoot180 <bhoot1806@gmail.com>
2017-03-30 18:09:29 -0400

    Add files via upload

---
+++ useCases/usecases/Usecase1.java
@@ -0,0 +1,28 @@
+// Depending on which code you comment you get the answer for usecase 1 or 2
+package Usecases;
+
+public class Usecase1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i = 5; i <= 50; i++){
+		
+			// Uncomment line 12 for usecase 1
+			 //System.out.println(i);
+
+			
+			// Uncomment lines 17-20 for usecase 2
+			
+			 /* if(i % 2 == 0) {
+			  
+			 	 	System.out.println(i); 
+				}
+			*/
+				
+		}
+		
+
+	}
+
+}

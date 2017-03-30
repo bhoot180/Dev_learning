8049dbfe4e1b0d2299205b07cef0e5a9b880fabb
bhoot180 <bhoot1806@gmail.com>
2017-03-30 17:59:41 -0400

    Add files via upload

---
+++ udacity/rectangleDemo/RectangleDemo.java
@@ -0,0 +1,21 @@
+package rectangledemo;
+
+import java.awt.Rectangle;
+
+public class RectangleDemo
+{
+    public static void main(String[] args)
+    {
+       // TODO: your code here
+      
+      Rectangle box = new Rectangle(60, 90, 20, 30);
+     
+      Rectangle secondBox = new Rectangle(80, 120, 20, 30);
+     
+      
+      box.draw();
+      secondBox.draw();
+      
+       
+    }
+}

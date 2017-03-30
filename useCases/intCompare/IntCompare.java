ad38ffcec31626089ecdde273921e898c60cd434
bhoot180 <bhoot1806@gmail.com>
2017-03-30 18:12:13 -0400

    Add files via upload

---
+++ useCases/intCompare/IntCompare.java
@@ -0,0 +1,33 @@
+package intCompare;
+
+public class IntCompare {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x = 100;
+				
+		if (x == 10) {
+			
+			System.out.println("Compared " + x + " with 10");
+		}
+		else if (x == 50) {
+			System.out.println("Compared " + x + " with 50");
+		}
+		
+		else if (x == 200) {
+			System.out.println("Compared " + x + " with 200");
+		}
+		
+		else if (x == 100) {
+			System.out.println("Compared " + x + " with 100");
+		}
+		
+		else {
+			
+			System.out.println("Compared " + x + " with nothing");
+		}
+
+	}
+
+}

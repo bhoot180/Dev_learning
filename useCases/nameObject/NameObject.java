18226f24d717a05e67caa799294cefc0fe93a9a3
bhoot180 <bhoot1806@gmail.com>
2017-03-30 18:16:46 -0400

    Add files via upload

---
+++ useCases/nameObject/NameObject.java
@@ -0,0 +1,22 @@
+// Create a class A with a property name and class B 
+// and create the instance of class A in B and set the name in the object A.
+
+package nameobject;
+
+public class NameObject {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	
+		SecondObject newObject = new SecondObject();
+		newObject.setSecondObject(10);
+		
+		System.out.println(newObject.getSecondObject());
+		
+	}
+	
+	
+
+}
---
+++ useCases/nameObject/SecondObject.java
@@ -0,0 +1,25 @@
+package nameobject;
+
+public class SecondObject {
+	
+	private int secondObject;
+
+	/**
+	 * @return the secondObject
+	 */
+	public int getSecondObject() {
+		return secondObject;
+	}
+
+	/**
+	 * @param secondObject the secondObject to set
+	 */
+	public void setSecondObject(int secondObject) {
+		this.secondObject = secondObject;
+	}
+		
+		 
+		
+
+
+}
